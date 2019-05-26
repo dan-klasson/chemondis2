a93960c205febe28c093d66c4261051c0a7f77a9
Daniel Klasson <dan.g.klasson@gmail.com>
2019-05-26 19:51:14 +0800

    switched to postgres

--- chemondis/settings.py
+++ chemondis/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'chemondis.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'chemondis',
+        'USER': 'daniel',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
